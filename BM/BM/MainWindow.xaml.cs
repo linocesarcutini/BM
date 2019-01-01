8e7ef75f92448d836ec3b1c5b3ab47d9089fa81d
Lino Cesar Cutini <cesarcutini@gmail.com>
2019-01-01 08:39:43 -0200

    Changes

--- BM/BM/MainWindow.xaml.cs
+++ BM/BM/MainWindow.xaml.cs
@@ -43,19 +43,59 @@ namespace BM {
                 tbUnicaSigla.IsEnabled = true;
             }
         }
-    }
 
-    public class Bolt {
-        public string Diametro { get; set; }
-        public string Qualidade { get; set; }
-        public int QTPorca { get; set; }
-        public string QualPorca { get; set; }
-        public string EspArruela { get; set; }
-        public string QTArruela { get; set; }
-        public string QualArruela { get; set; }
-        public string EspArruelaP { get; set; }
-        public string QTArruelaP { get; set; }
-        public string QualArruelaP { get; set; }
-        public string Palnut { get; set; }
+        private void DgParafusos_previewKeyDown(object sender, KeyEventArgs e) {
+            DataGrid grid = (DataGrid)sender;
+
+            if (e.Key == Key.Enter || e.Key == Key.Return) {
+                // get the selected row
+                DataGridRow selectedRow = grid.ItemContainerGenerator.ContainerFromItem(grid.SelectedItem) as DataGridRow;
+
+                int columnIndex = grid.SelectedCells[0].Column.DisplayIndex;
+                int rowIndex = grid.Items.IndexOf(grid.SelectedCells[0].Item) - 1;
+
+                // selectedRow can be null due to virtualization
+                if (selectedRow != null) {
+
+                    // there should always be a selected cell
+                    if (grid.SelectedCells.Count != 0) {
+                        // get the cell info
+                        DataGridCellInfo currentCell = grid.SelectedCells[0];
+
+                        // get the display index of the cell's column + 1 (for next column)
+                        int columnDisplayIndex = currentCell.Column.DisplayIndex++;
+
+                        // if display index is valid
+                        if (columnDisplayIndex < grid.Columns.Count) {
+                            // get the DataGridColumn instance from the display index
+                            DataGridColumn nextColumn = grid.ColumnFromDisplayIndex(columnDisplayIndex);
+
+                            // now telling the grid, that we handled the key down event
+                            e.Handled = true;
+
+                            // setting the current cell (selected, focused)
+                            grid.CurrentCell = new DataGridCellInfo(grid.SelectedItem, nextColumn);
+
+                            // tell the grid to initialize edit mode for the current cell
+                            grid.BeginEdit();
+                        }
+                    }
+                }
+            }
+        }
+
+        public class Bolt {
+            public string Diametro { get; set; }
+            public string Qualidade { get; set; }
+            public int QTPorca { get; set; }
+            public string QualPorca { get; set; }
+            public string EspArruela { get; set; }
+            public string QTArruela { get; set; }
+            public string QualArruela { get; set; }
+            public string EspArruelaP { get; set; }
+            public string QTArruelaP { get; set; }
+            public string QualArruelaP { get; set; }
+            public string Palnut { get; set; }
+        }
     }
 }
